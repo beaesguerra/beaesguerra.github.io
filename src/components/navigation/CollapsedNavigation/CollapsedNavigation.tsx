9520c2622bcdf180348e7c6fe937043ee8cbfc43
Bea Esguerra <bea.esguerra@outlook.com>
2020-03-05 19:49:04 -0700

    set up menu for mobile nav

--- src/components/navigation/CollapsedNavigation/CollapsedNavigation.tsx
+++ src/components/navigation/CollapsedNavigation/CollapsedNavigation.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { makeStyles, IconButton } from '@material-ui/core';
+import { makeStyles, IconButton, Menu, MenuItem } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { IBaseNavigationProps } from '../types';
@@ -7,12 +7,32 @@ import { Logo } from '../../basic/Logo/logo';
 
 export const CollapsedNavigation = (props: IBaseNavigationProps) => {
   const classes = useStyles();
+  const [opened, setOpened] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | Element>(null);
+
+  const handleOpen = (event: React.MouseEvent) => {
+    setOpened(true);
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setOpened(false);
+    setAnchorEl(null);
+  };
+
   return (
     <div className={classes.header}>
       <Logo />
-      <IconButton>
+      <IconButton onClick={handleOpen}>
         <FontAwesomeIcon icon={faBars} color="#ffffff" />
       </IconButton>
+      <Menu open={opened} anchorEl={anchorEl} onClose={handleClose}>
+        {props.links.map((link) => (
+          <MenuItem onClick={handleClose}>
+            {link.label}
+          </MenuItem>
+        ))}
+      </Menu>
     </div>
   );
 };
