596c8e031d5f75d8a9955ff81025ff425a8675c0
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-03 21:48:32 -0700

    set up contact info links

---
+++ src/components/design-system/icon-link.tsx
@@ -0,0 +1,23 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import * as React from 'react';
+import Icon from './icon';
+
+type IconLinkProps = {
+  href: string;
+  ariaLabel: string;
+  icon: IconProp;
+}
+
+const IconLink = (props: IconLinkProps) => (
+  <a
+    href={props.href}
+    aria-label={props.ariaLabel}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="hover:opacity-75 p-3"
+  >
+    <Icon icon={props.icon} size="md" />
+  </a>
+);
+
+export default IconLink;
