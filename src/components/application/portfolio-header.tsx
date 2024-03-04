de13fec52615ad35a502d3f5b5839dc9c9626e2e
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-03 20:02:53 -0700

    make header white for now

--- src/components/application/portfolio-header.tsx
+++ src/components/application/portfolio-header.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 const PortfolioHeader = () => {
   return (
-    <nav className="w-screen h-16 sticky top-0 px-56 flex justify-between items-center ">
+    <nav className="w-screen h-16 sticky top-0 px-56 flex justify-between items-center bg-white">
       <div className="text-3xl">bea esguerra</div>
       <div>
         email
