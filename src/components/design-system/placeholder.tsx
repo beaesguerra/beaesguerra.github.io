76050dff897e4a3f1001785159b59cbf1280c8a1
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-03 22:32:50 -0700

    create placeholder component that's useful for testing in the design systems page

---
+++ src/components/design-system/placeholder.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+type PlaceholderProps = {
+}
+
+const Placeholder = (props: PlaceholderProps) => (
+  <div className="bg-gray-200 w-full h-full min-h-40" />
+);
+
+export default Placeholder;
