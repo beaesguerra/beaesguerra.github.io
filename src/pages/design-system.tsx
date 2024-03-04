e2090a30b4871ade60d32e0cca9c43d293960ced
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-03 22:33:32 -0700

    add design system components to example page

--- src/pages/design-system.tsx
+++ src/pages/design-system.tsx
@@ -5,17 +5,20 @@ import Display, { DisplayVariant } from "../components/design-system/display"
 import StackList from "../components/design-system/stack-list"
 import Icon, { IconSize } from "../components/design-system/icon"
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import Illustration from "../components/design-system/illustration"
+import LayoutTwoThirds from "../components/design-system/layout-sidebar"
+import Placeholder from "../components/design-system/placeholder"
 
 const DesignSystem: React.FC<PageProps> = () => {
   const displayVariants: DisplayVariant[] = ['xs', 'sm', 'md', 'lg', 'xl']
   const iconSizes: IconSize[] = ['sm', 'md', 'lg']
-
+  const sectionClassName = 'uppercase font-bold text-xl py-4'
   return (
     <Layout>
       <h1 className="py-6 text-6xl">Design System </h1>
       <StackList>
         <div>
-          <h2 className="uppercase font-bold text-xl py-4">Typography</h2>
+          <h2 className={sectionClassName}>Typography</h2>
           <ul>
           { displayVariants.map((variant) => (
             <li>
@@ -27,7 +30,7 @@ const DesignSystem: React.FC<PageProps> = () => {
           </ul>
         </div>
         <div>
-          <h2 className="uppercase font-bold text-xl py-4">Icons</h2>
+          <h2 className={sectionClassName}>Icons</h2>
           <ul>
             {iconSizes.map((size) => (
               <li>
@@ -35,6 +38,15 @@ const DesignSystem: React.FC<PageProps> = () => {
               </li>
             ))}
           </ul>
+        </div>
+        <div>
+          <h2 className={sectionClassName}>Illustration</h2>
+          <Illustration icon={faHeart} />
+        </div>
+
+        <div>
+          <h2 className={sectionClassName}>Layout - Two Thirds</h2>
+          <LayoutTwoThirds main={(<Placeholder />)} sidebar={(<Placeholder />)} />
         </div>
       </StackList>
     </Layout>
