799a3b9dbf7f35c71b288ad7d7c3addd7a5a6b5f
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-03 22:34:02 -0700

    update education section

--- src/sections/education.tsx
+++ src/sections/education.tsx
@@ -1,20 +1,32 @@
 import * as React from 'react'
 import Section from '../components/application/section'
 import Display from '../components/design-system/display'
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+import Illustration from '../components/design-system/illustration'
+import LayoutSidebar from '../components/design-system/layout-sidebar'
 
 const EducationSection = () => {
   return (
     <Section>
       <Display variant="lg" tag="h1">Education</Display>
-      <Display variant="md" tag="h2">
-        University of Calgary (Schulich School of Engineering)
-      </Display>
-      <Display variant="sm" tag="h3">
-        Bachelor of Science in Software Engineering
-      </Display>
-      <p>
-        Graduated with distinction and successfully completed the Engineering Internship Program.
-      </p>
+      <LayoutSidebar
+        main={(
+          <>
+            <Display variant="md" tag="h2">
+              Bachelor of Science in Software Engineering
+            </Display>
+            <Display variant="sm" tag="h3">
+              University of Calgary (Schulich School of Engineering)
+            </Display>
+            <p>
+              Graduated with distinction and completed the Engineering Internship Program.
+            </p>
+          </>
+        )}
+        sidebar={(
+          <Illustration icon={faGraduationCap} />
+        )}
+      />
     </Section>
   )
 }
