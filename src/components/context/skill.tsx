d446de423a2a95e8119f17b932dc9e5b727fc2c0
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-04 20:52:40 -0700

    update skill image sizing

--- src/components/context/skill.tsx
+++ src/components/context/skill.tsx
@@ -8,7 +8,7 @@ type SkillProps = {
 
 const Skill = (props: SkillProps) => (
   <div className="flex flex-col items-center justify-center gap-2">
-    <img src={props.img} role="presentation" aria-hidden="true" className="h-16 md:h-14 object-contain" />
+    <img src={props.img} role="presentation" aria-hidden="true" className="h-16 object-contain" />
     <Text variant="sm" tag="span" className="text-center h-8">
       {
         props.label
