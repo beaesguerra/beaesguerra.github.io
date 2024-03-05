57639eb8fc91dee04fd3b6edcb788156552524b0
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-04 19:32:40 -0700

    increase spacing in banner

--- src/sections/hero-banner.tsx
+++ src/sections/hero-banner.tsx
@@ -4,7 +4,7 @@ import Display from '../components/design-system/display'
 
 const HeroBanner = () => {
   return (
-    <Section>
+    <Section className="py-16">
       <Display variant="xl" tag="h1">Hello world!</Display> 
       <p>My name is Bea and I am a software developer passionate about frontend development, design systems, accessibility, and learning! I enjoy using technology to create innovative solutions that help people!</p>
     </Section>
