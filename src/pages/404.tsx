3272927770ed31c21fdf55470c62b40f541eba29
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-06 02:26:47 -0700

    apply layout to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,47 +1,21 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
 import GlobalHead from "../components/application/global-head"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import Layout from "../components/application/layout"
+import Section from "../components/application/section"
+import Display from "../components/design-system/display"
+import Text from "../components/design-system/text"
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <Section className="flex items-center justify-center p-12">
+        <Display tag="h1" variant="lg">Page not found</Display>
+        <Text tag="p">
+          Sorry 😔, we couldn’t find what you were looking for!
+        </Text>
+      </Section>
+    </Layout> 
   )
 }
 
