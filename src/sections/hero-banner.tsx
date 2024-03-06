e2fd4b99e72326227dc1cfbbc3e8453877e7569c
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-06 01:56:18 -0700

    add a gradient to the hero banner

--- src/sections/hero-banner.tsx
+++ src/sections/hero-banner.tsx
@@ -5,8 +5,8 @@ import Section from '../components/application/section'
 
 const HeroBanner = () => {
   return (
-    <div className="bg-rose-200 w-full">
-      <Section className="md:min-h-[75vh] pt-4 md:pt-10 md:mt-0 mb-40 text-center justify-center md:w-3/4 m-auto pb-20">
+    <div className="bg-gradient-to-br from-rose-100 to-rose-300 w-full">
+      <Section className="md:min-h-[75vh] pt-10 md:pt-10 md:mt-0 mb-40 text-center justify-center md:w-3/4 m-auto pb-20">
         <div className="bg-white/95 rounded-3xl md:w-4/6 mx-auto px-4 py-8 md:py-12">
           <Display variant="xl" tag="h1">Hello world!</Display> 
           <Text tag="p">
