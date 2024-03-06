c5db60377ce99f0457e403432fa1091c9657c8c1
Bea Esguerra <bea.esguerra@outlook.com>
2024-03-06 00:12:43 -0700

    Update work experience content

--- src/sections/work-experience.tsx
+++ src/sections/work-experience.tsx
@@ -7,6 +7,7 @@ import BenevityLogo from '../images/benevity-logo.png';
 import OpenhouseAiLogo from '../images/openhouse-ai-logo.png'
 import PasonLogo from '../images/pason-logo.jpg'
 import Text from '../components/design-system/text';
+import ExternalLink from '../components/design-system/external-link';
 
 const WorkExperienceSection = () => {
   const workBlocks: WorkBlockProps[] = [
@@ -24,19 +25,62 @@ const WorkExperienceSection = () => {
       roles: [
         {
           title: 'Frontend Developer, Design Systems (Software Developer III)',
-          description: (<>Lorem ipsum</>),
-          keywords: [],
+          description: (
+            <>
+              <Text tag="p">
+                Created tokens, styling utilities, components, and documentation for <ExternalLink href="https://skyline.benevity.org/">Benevity’s design system, Skyline</ExternalLink>.
+                Collaborated closely with the design system designer to clarify requirements, ensure consistency between Figma and code components, and provide feedback around accessibility considerations, edge cases, and technical constraints.
+              </Text>
+              <Text tag="p">  
+                Established a process for building design system components that fulfill requirements for: visual styling, accessibility, responsiveness, browser support, code quality, testing, performance, content, internationalization, and documentation.
+              </Text>
+              <Text tag="p">
+                Developed unit tests, end-to-end tests for interactions and keyboard navigation, and visual regression tests.
+                Performed code maintenance, including a substantial migration from Vue 2 to Vue 3.
+              </Text>
+              <Text tag="p">
+                Provided reliable support for developers and designers on how to best use the design system.
+                Mentored developers that were keen to develop their frontend skills.
+                Advocated for accessibility with the Product Experience Inclusive Design Group to raise awareness and bring accessibility into processes.
+              </Text>
+            </>
+          ),
+          keywords: [
+            'Design system',
+            'Accessibility',
+            'TypeScript',
+            'Vue',
+            'Sass',
+            'Tokens',
+            'VuePress',
+            'Storybook',
+            'Chromatic',
+            'BackstopJS',
+            'Jest',
+            'Cypress',
+            'Figma',
+            'Documentation',
+            'Process',
+            'Developer support',
+            'Mentorship',
+          ],
         },
         {
           title: 'Software Developer II',
           description: (
-            <Text tag="p">
-              Developed features for a white-label web application for workplace giving with
-              responsiveness, accessibility, localization, and browser compatibility in mind.
-              Created reusable and modular components covered by thorough automated unit tests.
-              Improved accessibility on numerous parts of the product by applying appropriate
-              tags/attributes and ensuring that the navigational flow for screen readers is intuitive.
-            </Text>
+            <>
+              <Text tag="p">
+                Developed features for a white-label web application for workplace giving with
+                responsiveness, accessibility, localization, and browser compatibility in mind.
+                Created reusable and modular components covered by thorough automated unit tests.
+              </Text>
+              <Text>
+                Wrote JMeter load tests for a microservice to gather baseline performance metrics.
+              </Text>
+              <Text tag="p">
+                Remediated accessibility issues on numerous parts of the product to adhere to WCAG 2.1 Level A & AA.
+              </Text>
+            </>
           ),
           keywords: [
             'JavaScript',
@@ -66,25 +110,28 @@ const WorkExperienceSection = () => {
         {
           title: 'Full Stack Software Developer',
           description: (
-            <Text tag="p">
-              Established frontend architecture for multiple web applications:
-              a white-label product for home builders, a content management system,
-              and a custom analytics dashboard.
-              Developed an internal React component package to support customization in
-              the white-label application.
-              Implemented microservices with REST APIs,
-              setup continuous integration for automated testing and deployment workflows.
-              Created mockups in Figma to communicate ideas to the team about product feature designs,
-              user workflows, and data visualizations.
-              Led scrum for a team of six, and
-              contributed to the company's hiring process for frontend developers.
-            </Text>
+            <>
+              <Text tag="p">
+                Established frontend architecture for multiple web applications:
+                a white-label product for home builders, a content management system,
+                and a custom analytics dashboard.
+                Developed an internal React component package to support customization in
+                the white-label application. Created mockups in Figma to communicate ideas to the team about product feature designs,
+                user workflows, and data visualizations.
+              </Text>
+              <Text tag="p">
+                Integrated CircleCI workflows to run automated tests, versioning, and deployments. Implemented microservices with REST APIs and event driven architecture,
+              </Text>
+              <Text tag="p">
+                Led Scrum for a team of six, mentored entry-level developers on software development best practices and processes, and
+                contributed to the company's hiring process for frontend developers.
+              </Text>
+            </>
           ),
           keywords: [
             'TypeScript',
             'React',
             'Redux',
-            'Redux-Saga',
             'Material-UI',
             'Storybook',
             'Jest',
@@ -97,6 +144,7 @@ const WorkExperienceSection = () => {
             'CircleCI',
             'Figma',
             'Scrum Master',
+            'Mentorship',
           ],
         }
       ]
@@ -117,7 +165,7 @@ const WorkExperienceSection = () => {
           title: 'DataHub Software Developer Intern',
           description: (
             <Text tag="p">
-              Worked closely with a UX team to implement features for a drilling data web application
+              Worked closely with the User Experience team to implement features for a drilling data web application
               called <a href="https://www.pason.com/products/datahub-with-pason-live" className="underline">Pason Live</a>. Also, implemented a backend service for running scheduled
               tasks for customers, and integrated tools to improve the quality of microservices.
             </Text>
